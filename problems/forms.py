9e95f55dfa4baede7c15371cc12139c05b25283b
Dmitriy Novozhilov <dimon2x@gmail.com>
2017-08-06 17:19:13 +0300

    MAINT: little fix

--- problems/forms.py
+++ problems/forms.py
@@ -1,23 +1,10 @@
-import latex2mathml.converter
-import logging
-import re
-
 from django import forms
-
-
+from problems.templatetags.problems_extras import convert_to_mathml
 
 
 class EditableTexTextarea(forms.Textarea):
     template_name = 'tex_textarea.html'
 
     def render(self, name, value, attrs=None, renderer=None):
-        splitted = re.split(r'(\$.*?\$)', value)
-        for i in range(1, len(splitted), 2):
-            try:
-                splitted[i] = latex2mathml.converter.convert(splitted[i][1:-1])
-            except Exception as e:
-                logger.exception(e)
-                splitted[i] = f'<b><i>incorrect LaTeX input {splitted[i]}</i></b>'
-        html_value = ''.join(splitted)
-        attrs['math'] = html_value
+        attrs['math'] = convert_to_mathml(value)
         return super().render(name, value, attrs, renderer)
