f043c9c71f9385ed70939ad14c2746664e92b13f
Dmitriy Novozhilov <dimon2x@gmail.com>
2017-08-19 17:43:01 +0300

    MAINT: style fixes

--- create_superuser.py
+++ create_superuser.py
@@ -1,12 +1,9 @@
 import os
 import django
-from django.conf import settings
-
 
 
 if __name__ == '__main__':
     os.environ['DJANGO_SETTINGS_MODULE'] = 'convenient_tasks.settings'
-    # settings.configure()
     django.setup()
     from django.db import IntegrityError
     from problems.models import User
