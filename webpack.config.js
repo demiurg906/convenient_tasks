66a7c1d16d335fe5ff3f662910a9ee5afd90dfcb
Dmitriy Novozhilov <dimon2x@gmail.com>
2017-08-21 18:56:55 +0300

    MAINT: little refactoring

--- webpack.config.js
+++ webpack.config.js
@@ -2,9 +2,9 @@ var path = require('path');
 
 module.exports = {
     devtool: 'source-map',
-    entry: './js/main_modules/task_search_main.js',
+    entry: './static/js/src/main_modules/task_search_main.js',
     output: {
         path: path.resolve(__dirname, 'static'),
-        filename: 'js/task_search_main.js'
+        filename: 'js/task_search_main.bundle.js'
     }
 };
