2b313198d2110eca2ee29ce223a80d0a33c01180
Dmitriy Novozhilov <dimon2x@gmail.com>
2017-08-25 19:59:00 +0300

    MAINT: little refactoring

--- static/js/src/main_modules/task_search_main.js
+++ static/js/src/main_modules/task_search_main.js
@@ -1,5 +1,3 @@
-//TODO: добавить проверку версии браузера
-
 import {initialize_slider, set_listeners_for_sections_dropdown} from "../modules/left_search_panel";
 import {
     get_new_list, initialize_tasks_list, receive_add_to_pool_message, receive_get_task_message,
@@ -19,20 +17,20 @@ $(document).ready(function() {
     initialize_slider();
     initialize_tasks_list();
 
+    function default_handler (data){
+        console.error('Incorrect message type: ' + data.message_type);
+    }
+
+    let handlers = {};
+    handlers[TASKS_LIST] = receive_task_list_message;
+    handlers[GET_TASK] = receive_get_task_message;
+    handlers[ADD_TO_POOL] = receive_add_to_pool_message;
+    handlers[NEW_POOL] = receive_new_pool_message;
+
     function receive_message(message) {
         let data = JSON.parse(message);
-        if (data.message_type === TASKS_LIST) {
-            receive_task_list_message(data)
-        } else if (data.message_type === GET_TASK) {
-            receive_get_task_message(data)
-        } else if (data.message_type === ADD_TO_POOL) {
-            receive_add_to_pool_message(data);
-        } else if (data.message_type === NEW_POOL) {
-            receive_new_pool_message(data)
-        }
-        else {
-            console.log.error('Incorrect message type: ' + message.message_type)
-        }
+        let handler = handlers[data.message_type] || default_handler;
+        handler(data);
     }
 });
 
