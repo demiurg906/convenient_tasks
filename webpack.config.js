08094912c4188dea70b65527ea7fe03062158c80
Dmitriy Novozhilov <dimon2x@gmail.com>
2017-08-25 21:06:18 +0300

    MAINT: empty files for pools page

--- webpack.config.js
+++ webpack.config.js
@@ -10,10 +10,19 @@ module.exports = [
         }
     },
     {
-    devtool: 'source-map',
-    entry: './static/js/src/main_modules/task_search_main.js',
-    output: {
-        path: path.resolve(__dirname, 'static'),
-        filename: 'js/task_search_main.bundle.js'
+        devtool: 'source-map',
+        entry: './static/js/src/main_modules/task_search_main.js',
+        output: {
+            path: path.resolve(__dirname, 'static'),
+            filename: 'js/task_search_main.bundle.js'
+        }
+    },
+    {
+        devtool: 'source-map',
+        entry: './static/js/src/main_modules/pools_main.js',
+        output: {
+            path: path.resolve(__dirname, 'static'),
+            filename: 'js/pools_main.bundle.js'
+        }
     }
-}];
+];
