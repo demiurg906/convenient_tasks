bfc3190947d8b2f18e44425af6f82c337a0e4e3d
Dmitriy Novozhilov <dimon2x@gmail.com>
2017-08-31 00:04:23 +0300

    BUG: little fix

--- convenient_tasks/urls.py
+++ convenient_tasks/urls.py
@@ -24,7 +24,7 @@ from django.views.generic import TemplateView
 from problems.views import task_search, task_detail, pools, pool_pdf
 
 urlpatterns = [
-    url(r'^admin/$', admin.site.urls),
+    url(r'^admin/', admin.site.urls),
     url(r'^login/$', auth.LoginView.as_view(template_name='problems/pages/login.html'), name='login'),
     url(r'^logout/$', auth.LogoutView.as_view(next_page='/login'), name='logout'),
     url(r'^tasks/$', task_search),
