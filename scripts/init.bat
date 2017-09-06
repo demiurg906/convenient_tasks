78fa09a662a7c72e9f6fdd1ac53a396d4c42f5ff
Dmitriy Novozhilov <dimon2x@gmail.com>
2017-09-07 00:00:56 +0300

    MAINT: init scripts updated

--- scripts/init.bat
+++ scripts/init.bat
@@ -1,1 +1,1 @@
-pip install scrapy django django-nested-inline django-tagging latex2mathml bootstrap-admin channels pdfkit
+pip install scrapy django django-nested-inline django-tagging latex2mathml bootstrap-admin channels Pillow scrapy_djangoitem pdfkit
---
+++ scripts/init.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+pip install scrapy django django-nested-inline django-tagging latex2mathml bootstrap-admin channels Piilow scrapy_djangoitem pdfkit
